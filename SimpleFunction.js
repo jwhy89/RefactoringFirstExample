b968bb8035e1aa652829bf9b0f0975ae5571718e
Jarvis Yang <jarvisgyang@gmail.com>
2019-08-02 23:02:19 -0500

    first commit; should have committed earlier; set up json files and running node on the .js file runs the function; next step is to start the actual refactoring process

---
+++ SimpleFunction.js
@@ -0,0 +1,99 @@
+var fs=require('fs');
+var dataI=fs.readFileSync('invoice.json', 'utf8');
+// var invoice=JSON.parse(dataI);
+let invoice = JSON.parse(fs.readFileSync('invoice.json').toString());
+// console.log(invoice);
+
+var dataP=fs.readFileSync('play.json');
+// var play=JSON.parse(dataP);
+var plays = JSON.parse(fs.readFileSync('play.json').toString());
+// console.log(play);
+
+// invoice = {
+//     customer: 'BigCo',
+//     performances: [{
+//             "playID": "hamlet",
+//             "audience": 55
+//         },
+//         {
+//             "playID": "asLike",
+//             "audience": 35
+//         },
+//         {
+//             "playID": "othello",
+//             "audience": 40
+//         }]
+// }
+
+// console.log('invoice', invoice);
+
+// play = {
+//     hamlet: {
+//         "name": "Hamlet",
+//         "type": "tragedy"
+//     },
+//     asLike: {
+//         "name": "As You Like It",
+//         "type": "comedy"
+//     },
+//     othello: {
+//         "name": "Othello",
+//         "type": "tragedy"
+//     }
+// }
+
+// console.log('play', play);
+
+
+function statement() {
+    
+    // let invoice = JSON.parse(fs.readFileSync('invoice.json').toString());
+    let totalAmount = 0;
+    let volumeCredits = 0;
+    console.log('in function statement invoice', invoice);
+    console.log('in function statement play', plays);
+    
+    let result = `Statement for ${invoice.customer}\n`;
+    const format = new Intl.NumberFormat("en-US", {
+        style: "currency",
+        currency: "USD",
+        minimumFractionDigits: 2
+    }).format;
+
+    for (let perf of invoice.performances) {
+        let play = plays[perf.playID];
+        let thisAmount = 0;
+
+        switch (play.type) {
+            case "tragedy":
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case "comedy":
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${play.type}`);
+        }
+
+        // add volume credits
+        volumeCredits += Math.max(perf.audience - 30, 0);
+        // add extra credit for every ten comedy attendees
+        if ("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+
+        // print line for this order
+        result += `  ${play.name}: ${format(thisAmount/100)} (${perf.audience} seats)\n`;
+        totalAmount += thisAmount;
+    }
+    result += `Amount owed is ${format(totalAmount/100)}\n`;
+    result += `You earned ${volumeCredits} credits\n`;
+    return result;
+}
+
+console.log('statement', statement());
